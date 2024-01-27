bdbbabdfc610c70327b29adfa26b8c70e06384af
adam-shen <113874468+adam-shen@users.noreply.github.com>
2024-01-27 02:05:38 +0000

    new file:   Computer Organization and Arch II/output/testmain 	new file:   Computer Organization and Arch II/testmain.c

---
+++ Computer Organization and Arch II/testmain.c
@@ -0,0 +1,15 @@
+#define _OPEN_SYS_ITOA_EXT
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+
+  printf("Hello World!\n");
+  printf("My Name is %s, I live in %s\n", "Adam Shen", "South Brunswick");
+  printf("Todays Date is 01/26/2023\n");
+  printf("The course number is 264-60 and Course name is Computer Organization and Arch II \n");
+
+  return 0;
+}
