4dff5947a16f8ef8331b6b6905ef7050c70e0b0a
adam-shen <113874468+adam-shen@users.noreply.github.com>
2024-02-10 01:49:27 +0000

    new file:   Computer Organization and Arch II/output/var_testing 	new file:   Computer Organization and Arch II/var_testing.c

---
+++ Computer Organization and Arch II/var_testing.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main() {
+
+    int x = 10;
+
+    double y = 10.5;
+
+    char z = 'A';
+
+    printf("\n int:%d \n\n", x);
+
+    printf("\n double:%.1f \n\n", y);
+
+    printf("\n character:%c \n\n", z);
+
+    return 0;
+}
